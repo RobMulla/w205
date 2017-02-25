dd4e1d8d34f81331a948e2089d00973b3a5c6fd2
Rob <rob.mulla@gmail.com>
2017-02-25 14:49:49 -0500

    adding transform hospital sql

---
+++ exercise_1/transforming/hospital_ranks.sql
@@ -0,0 +1,19 @@
+DROP TABLE hospital_ranked;
+
+CREATE TABLE hospital_ranked AS
+
+SELECT 
+Provider_Number,
+Hospital_Name,
+State,
+RANK() OVER (ORDER BY Process_Score DESC) process_rank,
+RANK() OVER (ORDER BY Outcomes_Score DESC) outcomes_rank,
+RANK() OVER (ORDER BY Care_Score DESC) care_rank,
+RANK() OVER (ORDER BY Safety_Score DESC) safety_rank,
+RANK() OVER (ORDER BY Cost_Reduction_Score DESC) cost_rank,
+RANK() OVER (ORDER BY Total_Score DESC) overall_rank
+
+FROM hospital_transformed
+
+ORDER BY overall_rank DESC
+;
--- exercise_1/transforming/hospital_transform.sql
+++ exercise_1/transforming/hospital_transform.sql
@@ -1,16 +1,18 @@
+DROP TABLE hospital_transformed;
+
 CREATE TABLE hospital_transformed AS
 
 SELECT 
-tps.Provider_ID
+tps.Provider_Number,
 tps.Hospital_Name,
 tps.State,
-tps.Unweighted_Normalized_Clinical_Care__Process_Domain_Score as Process_Score,
-tps.Unweighted_Normalized_Clinical_Care__Outcomes_Domain_Score as Outcomes_Score,
-tps.Unweighted_Patient_and_Caregiver_Centered_Experience_of_Care_Care_Coordination_Domain_Score as Care_Coordination_Score,	tps.Unweighted_Normalized_Safety_Domain_Score as Safety_Score,
-tps.Unweighted_Normalized_Efficiency_and_Cost_Reduction_Domain_Score as Cost_Reduction_Score,
-tps.Total_Performance_Score as Total_Score,
-hospitals.Hospital_overall_rating as Overall_Rating
+cast(tps.Unweighted_Normalized_Clinical_Care__Process_Domain_Score as float) as Process_Score,
+cast(tps.Unweighted_Normalized_Clinical_Care__Outcomes_Domain_Score as float) as Outcomes_Score,
+cast(tps.Unweighted_Patient_and_Caregiver_Centered_Experience_of_Care_Care_Coordination_Domain_Score as float) as Care_Score,	
+cast(tps.Unweighted_Normalized_Safety_Domain_Score as float) as Safety_Score,
+cast(tps.Unweighted_Normalized_Efficiency_and_Cost_Reduction_Domain_Score as float) as Cost_Reduction_Score,
+cast(tps.Total_Performance_Score as float) as Total_Score,
+cast(hospitals.Hospital_overall_rating as decimal(1,0)) as Overall_Rating
 
-
-FROM tps JOIN ON hospitals WHERE tps.Provider_Number = hospitals.Provider_ID
+FROM tps INNER JOIN hospitals ON tps.Provider_Number = hospitals.Provider_ID
 ;
