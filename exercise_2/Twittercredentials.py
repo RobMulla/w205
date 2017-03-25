c7811fe7c66ecbbbb34fb680c4b7db42ad619a95
robmulla <rob.mulla@gmail.com>
2017-03-25 18:39:02 -0400

    twitter creds

--- exercise_2/Twittercredentials.py
+++ exercise_2/Twittercredentials.py
@@ -1,16 +1,16 @@
 import tweepy
 
-consumer_key = "YourConsumerkey";
+consumer_key = "nAwbOmzUnDOvj4wQr2aVEZAUZ";
 #eg: consumer_key = "YisfFjiodKtojtUvW4MSEcPm";
 
 
-consumer_secret = "YourConsumerSecret";
+consumer_secret = "lXhNRWqngJNmVyi5xaUS51YxRYbeV5OMYR0yduqCpUkMs0MXiF";
 #eg: consumer_secret = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
-access_token = "YourAcessToken";
+access_token = "14313217-5na24mGCcRAljaf9RN8SILrIkzscf9qS0SlD5XclS";
 #eg: access_token = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
-access_token_secret = "YourTokenSecret";
+access_token_secret = "qYxwOTs2GL2ESFyyzdLZDjpXR2aYEiNG7yWF8QVDPjt9F";
 #eg: access_token_secret = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
 
