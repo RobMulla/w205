53b129e058e7ca76394517297651dfabcdfe3806
Rob <rob.mulla@gmail.com>
2017-03-31 21:18:02 -0400

    first attempt at wordcount.py

--- exercise_2/extweetwordcount/src/bolts/wordcount.py
+++ exercise_2/extweetwordcount/src/bolts/wordcount.py
@@ -3,15 +3,56 @@ from __future__ import absolute_import, print_function, unicode_literals
 from collections import Counter
 from streamparse.bolt import Bolt
 
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+
+
 
 
 class WordCounter(Bolt):
 
     def initialize(self, conf, ctx):
         self.counts = Counter()
+        
+        conn = psycopg2.connect(database="postgres", user="postgres", password="pass", host="localhost", port="5432")
+
+        try:
+            # CREATE DATABASE can't run inside a transaction
+            conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+            cur = conn.cursor()
+            cur.execute("CREATE DATABASE tcount")
+            cur.close()
+            conn.close()
+        except:
+            print "Could not create tcount"
+
+        # Connect to tcount
+
+        conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
+
+        #Create a Table
+        #The first step is to create a cursor. 
+
+        cur = conn.cursor()
+        cur.execute('''CREATE TABLE tweetwordcount
+            (word TEXT PRIMARY KEY     NOT NULL,
+            count INT     NOT NULL);''')
+        conn.commit()
 
     def process(self, tup):
         word = tup.values[0]
+
+        cur = conn.cursor()
+
+        if self.counts[word] == 1:
+            # Insert the word into the table
+            cur.execute("INSERT INTO tweetwordcount (word,count) \
+                VALUES (word, self.counts[word])");
+            conn.commit()
+
+
+        cur.execute("UPDATE tweetwordcount SET count=%s WHERE word=%s", (self.counts[word], word))
 
         # Write codes to increment the word count in Postgres
         # Use psycopg to interact with Postgres
@@ -19,7 +60,6 @@ class WordCounter(Bolt):
         # Table name: Tweetwordcount 
         # you need to create both the database and the table in advance.
         
-
         # Increment the local count
         self.counts[word] += 1
         self.emit([word, self.counts[word]])
