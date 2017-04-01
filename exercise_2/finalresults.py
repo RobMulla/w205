012fbfa8332e185d98aea1ab90e264fedfa81d2f
Rob <rob.mulla@gmail.com>
2017-03-31 21:26:34 -0400

    add final results and histogram files2

--- exercise_2/finalresults.py
+++ exercise_2/finalresults.py
@@ -4,6 +4,7 @@ from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 #Connecting to tcount
 conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
 
+cur = conn.cursor()
 cur.execute("SELECT word, count from tweetwordcount")
 records = cur.fetchall()
 for rec in records:
