5b0c1e1f7a2e1f0be7a673c9d023318016bbcf52
Rob <rob.mulla@gmail.com>
2017-03-31 21:45:23 -0400

    add final results and histogram files

--- exercise_2/extweetwordcount/src/bolts/wordcount.py
+++ exercise_2/extweetwordcount/src/bolts/wordcount.py
@@ -17,10 +17,12 @@ class WordCounter(Bolt):
         
         conn = psycopg2.connect(database="postgres", user="postgres", password="pass", host="localhost", port="5432")
 
+
         try:
             # CREATE DATABASE can't run inside a transaction
             conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
             cur = conn.cursor()
+            cur.execute("DROP DATABASE tcount")
             cur.execute("CREATE DATABASE tcount")
             cur.close()
             conn.close()
