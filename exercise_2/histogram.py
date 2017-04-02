2548bcca67dc15e941c03d29c900d3cd8ea87dd0
Rob <rob.mulla@gmail.com>
2017-04-02 18:16:44 -0400

    Editing histogram file

---
+++ exercise_2/histogram.py
@@ -0,0 +1,28 @@
+import sys
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+
+#Connecting to tcount
+conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
+
+cur = conn.cursor()
+cur.execute("SELECT word, count from tweetwordcount")
+records = cur.fetchall()
+records.sort(key = lambda tup: tup[0])
+
+try:
+	min = sys.argv[1:][0]
+	max = sys.argv[2:][1]
+except:
+	print "bad format. This program requires two numbers 1,2"
+else:
+	print "min is"
+	print min
+	print "max is"
+	print max
+#	for rec in records:
+#		if arg == rec[0]:
+#			print('Total number of occurrences of "%s": %s') % (rec[0], rec[1])
+
+conn.close()
