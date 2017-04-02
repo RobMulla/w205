32f43bc952cbd077c9625534c7d84ca34caeca48
Rob <rob.mulla@gmail.com>
2017-04-02 18:09:07 -0400

    add final results and histogram files

--- exercise_2/finalresults.py
+++ exercise_2/finalresults.py
@@ -2,7 +2,6 @@ import sys
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
-arg = sys.argv[1:][0]
 
 #Connecting to tcount
 conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
@@ -12,7 +11,13 @@ cur.execute("SELECT word, count from tweetwordcount")
 records = cur.fetchall()
 records.sort(key = lambda tup: tup[1])
 
-for rec in records:
-	if arg == rec[0]:
-		print('Total number of occurrences of "%s": %s') % (rec[0], rec[1])
-conn.commit()
+try:
+	arg = sys.argv[1:][0]
+except:
+	print records
+else:
+	for rec in records:
+		if arg == rec[0]:
+			print('Total number of occurrences of "%s": %s') % (rec[0], rec[1])
+
+conn.close()
