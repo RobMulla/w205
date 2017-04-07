5370a451e300660232d50e622bac5490b82d3b26
Rob <rob.mulla@gmail.com>
2017-04-07 18:00:55 -0400

    fix histogram

--- exercise_2/histogram.py
+++ exercise_2/histogram.py
@@ -7,18 +7,19 @@ try:
 	minmax = sys.argv[1:][0]
 	#print minmax
 	min, max = minmax.split(',')
+
+	#Connecting to tcount
+	conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
+	cur = conn.cursor()
+	cur.execute("SELECT word, count from tweetwordcount where count >= %s and count <= %s \
+				ORDER BY count DESC;", (min, max))
+	records = cur.fetchall()
+
+	for record in records:
+		print str(record[0]) + ": " + str(record[1])
+
 except:
 	print "bad format. This program requires two numbers 1,2"
 
-#Connecting to tcount
-conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
-
-cur = conn.cursor()
-cur.execute("SELECT word, count from tweetwordcount where count >= %s and count <= %s \
-			ORDER BY count DESC;", (min, max))
-records = cur.fetchall()
-
-for record in records:
-	print str(record[0]) + ": " + str(record[1])
-
+# Close the connection
 conn.close()
