092393a3f30610c613efd2a527c31253b7468c7d
Rob <rob.mulla@gmail.com>
2017-04-07 17:56:30 -0400

    making plot

--- exercise_2/plot.py
+++ exercise_2/plot.py
@@ -3,18 +3,12 @@ import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 # Pull in the min and max values
-try:
-	minmax = sys.argv[1:][0]
-	#print minmax
-	min, max = minmax.split(',')
-except:
-	print "bad format. This program requires two numbers 1,2"
 
 #Connecting to tcount
 conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
 
 cur = conn.cursor()
-cur.execute("SELECT word, count from tweetwordcount where count >= %s and count <= %s \
+cur.execute("SELECT word, count from tweetwordcount limit 20 \
 			ORDER BY count DESC;", (min, max))
 records = cur.fetchall()
 
