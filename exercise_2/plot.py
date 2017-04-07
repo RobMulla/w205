1799d0591f45de977dcb6c2ce95d0662ad059fc8
Rob <rob.mulla@gmail.com>
2017-04-07 18:06:04 -0400

    fix histogram

--- exercise_2/plot.py
+++ exercise_2/plot.py
@@ -2,6 +2,8 @@ import sys
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
+import matplotlib.pyplot as plt
+
 # Pull in the min and max values
 
 #Connecting to tcount
@@ -12,7 +14,25 @@ cur.execute("SELECT word, count from tweetwordcount  \
 			ORDER BY count DESC LIMIT 20;", (min, max))
 records = cur.fetchall()
 
+words_list = []
+counts_list = []
+
 for record in records:
 	print str(record[0]) + ": " + str(record[1])
+	words_list.append(record[0])
+	counts_list.append(record[1])
+
+print words_list
+print counts_list
+
+# y_pos = np.arange(len(objects))
+
+# plt.bar(records[1], performance, align='center', alpha=0.5)
+# plt.xticks(y_pos, objects)
+# plt.ylabel('Usage')
+# plt.title('Programming language usage')
+ 
+# plt.show()
+
 
 conn.close()
